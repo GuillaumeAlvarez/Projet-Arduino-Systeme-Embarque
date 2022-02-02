d44d437ffe54a38bd087883ee4465f87aa0ad4a7
GuillaumeAlvarez <alvarez.guillaume2506@gmail.com>
2022-02-02 11:01:15 +0100

    Affichage de plusieurs écrans avec heure, température, etc .. mais heure fonctionne pas bien encore

---
+++ src/message.cpp
@@ -0,0 +1,24 @@
+// #include <Arduino.h>
+// #include <M5StickC.h>
+
+// void setup() {
+//   // put your setup code here, to run once:
+
+
+//   // // Initialize the M5StickC object. Initialize the M5StickC object
+//   // M5.begin();
+
+//   // // LCD display. LCd display
+//   // M5.Lcd.print("Coucou toi !");
+  
+//   M5.begin();
+//   M5.Lcd.setRotation(3);
+//   M5.Lcd.fillScreen(RED);
+//   M5.Lcd.setTextSize(4);
+//   M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+//   M5.Lcd.print("Coucou toi !");
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+// }
