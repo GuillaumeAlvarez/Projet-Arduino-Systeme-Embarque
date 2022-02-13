dfc1cec159ed3ef2144750df322332710bc5221d
GuillaumeAlvarez <alvarez.guillaume2506@gmail.com>
2022-02-13 14:22:00 +0100

    partie capteur

--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,6 @@
 #include <Wire.h> //The DHT12 uses I2C comunication.
 #include "Adafruit_Sensor.h"
 #include <Adafruit_BMP280.h>
-#include <Arduino.h>
-#include <Tone32.h>
-
-#define BUZZER_CHANNEL 0
-#define BUZZER_PIN 0
-
-#define OCTAVE_4_DO 523
-#define OCTAVE_4_RE 587
-#define OCTAVE_4_MI 659
-#define OCTAVE_4_FA 349
-#define OCTAVE_4_SOL 392
-#define OCTAVE_4_LA 440
-#define OCTAVE_4_SI 494
-
-extern const unsigned char m5stack_startup_music[];
-
-const int servo_pin = 26;
-
-int freq = 50;
-int ledChannel = 0;
-int resolution = 10;
-int listNotes[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-int k = 0;
 
 DHT12 dht12; //Preset scale CELSIUS and ID 0x5c.
 Adafruit_BMP280 bme;
@@ -44,94 +21,6 @@ int battery = 0;
 float b = 0;
 int state = 1;
 
-void partitionEmptyWithKey()
-{
-  M5.Lcd.fillRect(20 /*y (haut en bas*/, 10, 2 /*longueur*/, 140 /*largeur */, BLACK);
-  M5.Lcd.fillRect(26 /*y (haut en bas*/, 10, 2 /*longueur*/, 140 /*largeur */, BLACK);
-  M5.Lcd.fillRect(32 /*y (haut en bas*/, 10, 2 /*longueur*/, 140 /*largeur */, BLACK);
-  M5.Lcd.fillRect(38 /*y (haut en bas*/, 10, 2 /*longueur*/, 140 /*largeur */, BLACK);
-  M5.Lcd.fillRect(44 /*y (haut en bas*/, 10, 2 /*longueur*/, 140 /*largeur */, BLACK);
-}
-
-void note(int note, int position)
-{
-  M5.Lcd.fillRect(note /*y (haut en bas*/, position, 6 /*longueur*/, 6 /*largeur */, BLACK);
-  M5.Lcd.fillRect(note - 10 /*y (haut en bas*/, position, 10 /*longueur*/, 2 /*largeur */, BLACK);
-}
-
-String showNote(int i)
-{
-  String action = "";
-  switch ((i - 20) / 3 % 7)
-  {
-  case 0:
-    ledcWriteTone(BUZZER_PIN, OCTAVE_4_MI);
-    delay(200);
-    ledcWriteTone(BUZZER_PIN, 0);
-    delay(200);
-    action = "MI";
-    break;
-  case 1:
-    ledcWriteTone(BUZZER_PIN, OCTAVE_4_RE);
-    delay(200);
-    ledcWriteTone(BUZZER_PIN, 0);
-    delay(200);    
-    action = "RE";
-    break;
-  case 2:
-    ledcWriteTone(BUZZER_PIN, OCTAVE_4_DO);
-    delay(200);
-    ledcWriteTone(BUZZER_PIN, 0);
-    delay(200);
-    action = "DO";
-    break;
-  case 3:
-    ledcWriteTone(BUZZER_PIN, OCTAVE_4_SI);
-    delay(200);
-    ledcWriteTone(BUZZER_PIN, 0);
-    delay(200);
-    action = "SI";
-    break;
-  case 4:
-    ledcWriteTone(BUZZER_PIN, OCTAVE_4_LA);
-    delay(200);
-    ledcWriteTone(BUZZER_PIN, 0);
-    delay(200);
-    action = "LA";
-    break;
-  case 5:
-    ledcWriteTone(BUZZER_PIN, OCTAVE_4_SOL);
-    delay(200);
-    ledcWriteTone(BUZZER_PIN, 0);
-    delay(200);
-    action = "SOL";
-    break;
-  case 6:
-    ledcWriteTone(BUZZER_PIN, OCTAVE_4_FA);
-    delay(200);
-    ledcWriteTone(BUZZER_PIN, 0);
-    delay(200);
-    action = "FA";
-    break;
-  }
-  return action;
-}
-
-void actualisePartition(){
-      M5.Lcd.fillScreen(TFT_WHITE);
-    partitionEmptyWithKey();
-    for (int j = 0; j < 8; j++)
-    {
-      if (listNotes[j] != 0)
-      {
-        note(listNotes[j], 140 - j * 15);
-      }else{
-        j = 8;
-      }
-    }
-    M5.Lcd.setCursor(0, 0);
-}
-
 void setTime() {
 
   // connexion au Wifi
@@ -144,11 +33,11 @@ void setTime() {
     delay(500); //délai de 0.5s
     M5.Lcd.print(".");
   }
-  while (WiFi.status() == WL_CONNECTED) { 
+  while (WiFi.status() == WL_CONNECTED) {  //if wifi does connect go ahead and set the time with NTP
 
     Serial.println(" CONNECTED, SETTING TIME");
 
-    configTime(1 * 3600, 3600, ntpServer); //1 est la zone Europe/Paris (utc +1) 
+    configTime(1 * 3600, 3600, ntpServer); //1 is for my timezone in Europe/Paris (utc +1) 
   
     // Get local time
     struct tm timeInfo;
@@ -203,6 +92,15 @@ void batteryPercent() {
   b = M5.Axp.GetVbatData() * 1.1 / 1000;
   battery = ((b - 3.0) / 1.2) * 100;
 
+  // if (battery > 100)
+  //   battery = 100;
+  // else if (battery < 100 && battery > 9)
+  //   M5.Lcd.print(" ");
+  // else if (battery < 9)
+  //   M5.Lcd.print("  ");
+  // if (battery < 10)
+  //   M5.Axp.DeepSleep();
+
   M5.Lcd.setTextColor(TFT_BLACK, TFT_GREEN);
   M5.Lcd.print(battery);
   M5.Lcd.print("%");
@@ -211,82 +109,13 @@ void batteryPercent() {
 //Fonction appelée au démarrage, exécuté une fois --> initialise les variables
 void setup() {
 
-  M5.begin();
-  M5.IMU.Init();
+  M5.begin(true,true,true);
   M5.Lcd.setRotation(3);
-  
-  M5.Axp.ScreenBreath(15); //permettre de mettre la niveau de luminosité élevé
+  // M5.Axp.ScreenBreath(8);  //screen brightness 7-15
+  M5.Axp.ScreenBreath(15); 
   M5.Lcd.fillScreen(BLACK);
   setTime();
-
   
-  ledcSetup(ledChannel, freq, resolution);
-  ledcAttachPin(servo_pin, ledChannel);
-  
-}
-
-void buttonAction()
-{
-  String action = "";
-  int i = 20;
-  if (digitalRead(M5_BUTTON_HOME) == LOW 
-  || digitalRead(BUTTON_B_PIN) == LOW)
-  {
-  actualisePartition();
-    if (digitalRead(M5_BUTTON_HOME) == LOW)
-    {
-      //action = "A";
-      while (digitalRead(M5_BUTTON_HOME) == LOW)
-      {
-        if (digitalRead(BUTTON_B_PIN) == LOW)
-        {
-          //action = "A B";
-          i += 3;
-          while (digitalRead(BUTTON_B_PIN) == LOW)
-          {
-            actualisePartition();
-            M5.Lcd.setRotation(3);
-            M5.Lcd.println(showNote(i));
-            M5.Lcd.setRotation(0);
-          }
-          delay(200);
-        }
-      }
-    }
-  
-    k = 0;
-    do
-    {
-      if (listNotes[k] == 0)
-      {
-        listNotes[k] = i;
-        note(listNotes[k], 140 - k * 15);
-        if(k == 8){
-          for(int m = 0; m < 8; m++){
-            delay(200);
-          }
-          ledcWriteTone(ledChannel, 0);
-          delay(1000);
-        }
-        k = 8;
-      }
-      k++;
-    } while (k < 8);
-
-    delay(200);
-  }
-  if(listNotes[7] != 0 && listNotes[8] == 0){
-    for(int b = 0; b < 4; b++){
-      for(int a = 0; a < 8; a++){
-        M5.Lcd.setRotation(3);
-        showNote(listNotes[a]);
-        M5.Lcd.setRotation(0);
-      }
-    }
-    for(int a = 0; a < 8; a++){
-      listNotes[a] = 0;
-    }
-  }
 }
 
 //loupe consécutivement afin que le programme réponde aux différentes actions
@@ -308,7 +137,17 @@ void loop() {
         
 
         } else if(modeButton == LOW && sleepButton == HIGH) {
-            state = 2;
+          // M5.Lcd.fillScreen(BLACK);    //Added visual indicator to add time between the button press and state change.
+          // M5.Lcd.setCursor(2, 25);
+          // M5.Lcd.setTextSize(1);
+          // M5.Lcd.printf("Passage 1");
+          // Serial.begin(115200);
+          // // Serial2.begin(unsigned long baud, uint32_t config, int8_t rxPin, int8_t txPin, bool invert)
+          // Serial2.begin(115200, SERIAL_8N1, 26, 0); //Sets the pin configuration for mode 2 serial pass through
+          // delay(1000);
+          // M5.Lcd.fillScreen(BLACK);
+          // M5.Lcd.setCursor(2, 2);
+          state = 2;
         } else if(modeButton == HIGH && sleepButton == LOW) {
           M5.Lcd.fillScreen(BLACK);
           M5.Axp.SetSleep();
@@ -320,17 +159,13 @@ void loop() {
   
           if (!bme.begin(0x76)){  
             Serial.println("Could not find a valid BMP280 sensor, check wiring!");
-            M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
-          M5.Lcd.fillScreen(BLACK);    //Added visual indicator to add time between the button press and state change.
-          M5.Lcd.setCursor(2, 25);
-          M5.Lcd.setTextSize(2);
-          M5.Lcd.printf("Passage en mode musique");
-            state = 4;
+            state = 1;
           }
   
           float tmp = dht12.readTemperature();
           float hum = dht12.readHumidity();
           float pressure = bme.readPressure();
+          // Serial.printf("Temperatura: %2.2f*C  Humedad: %0.2f%%  Pressure: %0.2fPa\r\n", tmp, hum, pressure);
 
           M5.Lcd.setCursor(0, 15);
           M5.Lcd.fillScreen(WHITE); 
@@ -341,18 +176,13 @@ void loop() {
           delay(1000);
           
         } else if(modeButton == LOW && sleepButton == HIGH) {
-          delay(1000);
-          
-        } else if(modeButton == LOW && sleepButton == HIGH) {
-          M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
-          M5.Lcd.fillScreen(BLACK);    //Added visual indicator to a
           M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
           M5.Lcd.fillScreen(BLACK);    //Added visual indicator to add time between the button press and state change.
           M5.Lcd.setCursor(2, 25);
           M5.Lcd.setTextSize(2);
-          M5.Lcd.printf("Passage en mode musique");
-          delay(4000);
-          state = 4;
+          M5.Lcd.printf("Retour a \r\nl'accueil");
+          delay(1000);
+          state = 1;
         } else if(modeButton == HIGH && sleepButton == LOW) {
           M5.Lcd.fillScreen(BLACK);
           M5.Axp.SetSleep();
@@ -396,23 +226,6 @@ void loop() {
               M5.Axp.SetSleep();
             }
       break;
-
-      case 4 :
-          M5.Lcd.setRotation(0);
-          buttonAction();
-          if(modeButton == LOW && sleepButton == HIGH) {
-          M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
-          M5.Lcd.fillScreen(BLACK);    //Added visual indicator to add time between the button press and state change.
-          M5.Lcd.setCursor(2, 25);
-          M5.Lcd.setTextSize(2);
-           M5.Lcd.setRotation(3);
-          M5.Lcd.printf("Retour a \r\nl'accueil");
-          delay(1000);
-          state = 1;
-        } else if(modeButton == HIGH && sleepButton == LOW) {
-          M5.Lcd.fillScreen(BLACK);
-        }
-          
    }
 }
 
